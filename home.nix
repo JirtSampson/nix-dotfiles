bb8a9505b0968af5fc47694ce7bf46046932c78d
Chris Esposito <esposito30@gmail.com>
2024-10-20 09:46:23 -0400

    Bringing nvim config into homemgr

--- home.nix
+++ home.nix
@@ -45,7 +45,12 @@
         };
     };
     jq.enable = true;
-    neovim = {
+    neovim =  
+    let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in
+    {
       enable = true;
       defaultEditor = true;
       extraLuaConfig = ''
