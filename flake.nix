6adef798927581b2fc15b153c8584ecfb268d048
Chris Esposito <esposito30@gmail.com>
2024-10-20 10:40:19 -0400

    Bringing nvim config into homemgr, fixed config paths

--- flake.nix
+++ flake.nix
@@ -18,12 +18,11 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
-      homeConfigurations."chris" = {
+      homeConfigurations."chris" = 
         inputs.home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ ./home.nix ];
 	  extraSpecialArgs = { inherit inputs; };
         };
-      };
     };
 }
